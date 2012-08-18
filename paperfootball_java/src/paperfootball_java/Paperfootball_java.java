13283d5137a595cb9acdf5fe8c521f8a6bc302aa
Vladislav <rabbiabram@gmail.com>
2012-08-18 16:32:29 +0700

    Default Netbeans Java Project

---
+++ paperfootball_java/src/paperfootball_java/Paperfootball_java.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paperfootball_java;
+
+/**
+ *
+ * @author абрам
+ */
+public class Paperfootball_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
