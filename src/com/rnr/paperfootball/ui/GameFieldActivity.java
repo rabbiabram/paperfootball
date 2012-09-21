fd3bc1054bb50e7f3f50beedc652b4110b508b58
rodnover <rodnover55@gmail.com>
2012-09-22 00:05:30 +0700

    Rename GameField to GameFieldActivity.

--- src/com/rnr/paperfootball/ui/GameFieldActivity.java
+++ src/com/rnr/paperfootball/ui/GameFieldActivity.java
@@ -1,8 +1,8 @@
 package com.rnr.paperfootball.ui;
 
+import com.rnr.paperfootball.LocalPlayer;
 import com.rnr.paperfootball.TestPlayer;
 import com.rnr.paperfootball.base.BaseMapBuilder;
-import com.rnr.paperfootball.base.BasePlayer;
 import com.rnr.paperfootball.core.Game;
 import com.rnr.paperfootball.core.InsufficientPlayersException;
 import com.rnr.paperfootball.map.MapBuilder;
@@ -11,7 +11,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
-public class GameField extends Activity {
+public class GameFieldActivity extends Activity implements PathGetter {
 
 	private Game mGame;
 	private BaseMapBuilder mGameBuilder;
@@ -32,7 +32,7 @@ public class GameField extends Activity {
         this.mGameFieldView.requestFocus();
 
         this.mGame.addPlayer(new TestPlayer());
-        this.mGame.addPlayer(new BasePlayer());
+        this.mGame.addPlayer(new LocalPlayer());
 
         try {
 			this.mGame.startGame();
